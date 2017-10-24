0ad3bdeb257f68ee03735654fe050d3d6a29f6d5
Usama Arshad <usama.lhr@gmail.com>
2017-10-24 16:29:52 +0500

    Fixed according to the question

--- homework.rb
+++ homework.rb
@@ -1,28 +1,37 @@
-def get_area_code_from_hash(hash)
+def get_area_code_from_hash(hash, city)
+  hash[city]
+end
+
+def display_city_from_hash(hash)
   puts 'Here are the list of cities'
   hash.each {|city| puts city}
+end
+
+dial_codes = {
+  'Lahore' => '042',
+  'Karachi' => '021',
+  'Islamabad' => '051',
+  'Narowal' => '010',
+  'Sialkot' => '020',
+  'Jaranwala' => '023',
+  'Nathiagali' => '024',
+  'Muree' => '025',
+  'Hunza' => '026',
+  'Ferry Meadows' => '027'
+}
+
+loop do
+  puts 'Do you want to search city codes (Y/N)'
+  continue = gets.chomp
+  break unless continue[/y/i]
+  display_city_from_hash(dial_codes)
   puts
   puts 'Choose the city whoose area code you want us to display'
   city = gets.chomp
-  hash.include?(city) ? puts("The code for #{city} is #{hash[city]}") : puts(city + ' is not a city in our record')
-  puts
-end
-
-loop do
-  dial_codes = {
-    'Lahore' => '042',
-    'Karachi' => '021',
-    'Islamabad' => '051',
-    'Narowal' => '010',
-    'Sialkot' => '020',
-    'Jaranwala' => '023',
-    'Nathiagali' => '024',
-    'Muree' => '025',
-    'Hunza' => '026',
-    'Ferry Meadows' => '027'
-  }
-  get_area_code_from_hash(dial_codes)
-  puts 'Do you want to continue? (Y/N)'
-  continue = gets.chomp
-  break unless continue[/y/i]
+  if dial_codes.include?(city)
+    code = get_area_code_from_hash(dial_codes, city)
+    puts "The code for #{city} is #{code}"
+  else
+    puts(city + ' is not a city in our record')
+  end
 end
